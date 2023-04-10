9fd81496e31675024c3838defdfa1558ff9a9f64
Highway-Star <93656490+Highway-Star@users.noreply.github.com>
2023-04-10 16:59:59 -0400

    s

--- Assets/Scripts/Reveal.cs
+++ Assets/Scripts/Reveal.cs
@@ -4,14 +4,24 @@ using UnityEngine;
 
 public class Reveal : MonoBehaviour
 {
-    public GameObject revealObject;
+    public GameObject[] revealObjects;
+    public GameObject[] concealObjects;
+
     bool isRevealed = false;
 
     private void OnTriggerEnter(Collider other)
     {
         if(isRevealed == false)
         {
-            revealObject.SetActive(true);
+            foreach(GameObject go in revealObjects)
+            {
+                go.SetActive(true);
+            }
+            foreach (GameObject co in concealObjects)
+            {
+                co.SetActive(false);
+            }
+            //revealObject.SetActive(true);
             isRevealed = true;
         }
     }
