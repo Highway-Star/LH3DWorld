354f0d7ca2069d17fb626d9ac30bda3dbad24ec3
Highway-Star <93656490+Highway-Star@users.noreply.github.com>
2023-05-01 19:08:58 -0400

    fes

--- Assets/Scripts/Target.cs
+++ Assets/Scripts/Target.cs
@@ -6,16 +6,19 @@ using UnityEngine.AI;
 public class Target : MonoBehaviour
 {
     
-    public Transform target;
+    public Transform target1;
+    public Transform target2;
+    public Transform currTarget;
     NavMeshAgent nav;
     private float timer = 0;
     [SerializeField]
     private float timertotal = 1;
-    bool started = false;
+    public bool started = false;
     // Start is called before the first frame update
     void Start()
     {
         nav = GetComponent<NavMeshAgent>();
+        currTarget = target1;
     }
 
     void Update()
@@ -38,12 +41,22 @@ public class Target : MonoBehaviour
     {
         started = true;
     }
+    public void TSwitch()
+    {
+        if(currTarget == target1)
+        {
+            currTarget = target2;
+        }
+        else { 
+            currTarget = target1;
+        }
+    }
     protected virtual void TimerContent()
     {
         if (started == true)
         {
             print("GO");
-            nav.SetDestination(target.position);
+            nav.SetDestination(currTarget.position);
         }
     }
 }
